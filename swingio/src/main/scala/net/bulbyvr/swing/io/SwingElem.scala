a66d4fc0d5a5d2be38f059950855b4dfcc0217c0
Noel Welsh <noel@noelwelsh.com>
2023-04-25 08:57:08 +0100

    Add slider component
    - Add `slider` component to the DSL, wrapping the existing `Slider`
    component
    - Add properties for `min`, `max`, and `value`. These are the most
    important properties for the slider.
    
    Other properties could be defined, but the most important ones are covered.

--- swingio/src/main/scala/net/bulbyvr/swing/io/SwingElem.scala
+++ swingio/src/main/scala/net/bulbyvr/swing/io/SwingElem.scala
@@ -10,34 +10,69 @@ import fs2.concurrent.Channel
 import wrapper as swingio
 sealed trait SwingElem[F[_], E, R] private[io] {
   def apply[M](modifier: M)(using M: Modifier[F, E, M]): Resource[F, R]
-  def withSelf[M](mkModifier: E => M)(using M: Modifier[F, E, M]): Resource[F, R]
-  
+  def withSelf[M](mkModifier: E => M)(using
+      M: Modifier[F, E, M]
+  ): Resource[F, R]
+
 }
-final class NormalElem[F[_], E] private[io] (val constructor: Resource[F, E])(using F: Async[F]) extends SwingElem[F, E, E] {
+final class NormalElem[F[_], E] private[io] (val constructor: Resource[F, E])(
+    using F: Async[F]
+) extends SwingElem[F, E, E] {
   def apply[M](modifier: M)(using M: Modifier[F, E, M]): Resource[F, E] =
     constructor.flatTap(M.modify(modifier, _))
-  def withSelf[M](mkModifier: E => M)(using M: Modifier[F, E, M]): Resource[F, E] =
+  def withSelf[M](mkModifier: E => M)(using
+      M: Modifier[F, E, M]
+  ): Resource[F, E] =
     constructor.flatTap(e => M.modify(mkModifier(e), e))
 }
-final class MainElem[F[_]] private[io] (val fullConstructor: Resource[F, (swingio.MainFrame[F], Deferred[F, Unit])])(using F: Async[F]) 
-  extends SwingElem[F, swingio.MainFrame[F], (swingio.MainFrame[F], Deferred[F, Unit])] {
-  override def apply[M](modifier: M)(using M: Modifier[F, swingio.MainFrame[F], M]): Resource[F, (swingio.MainFrame[F], Deferred[F, Unit])] =
+final class MainElem[F[_]] private[io] (
+    val fullConstructor: Resource[F, (swingio.MainFrame[F], Deferred[F, Unit])]
+)(using F: Async[F])
+    extends SwingElem[F, swingio.MainFrame[
+      F
+    ], (swingio.MainFrame[F], Deferred[F, Unit])] {
+  override def apply[M](modifier: M)(using
+      M: Modifier[F, swingio.MainFrame[F], M]
+  ): Resource[F, (swingio.MainFrame[F], Deferred[F, Unit])] =
     fullConstructor.flatTap((l, _) => M.modify(modifier, l))
-  override def withSelf[M](mkModifier: swingio.MainFrame[F] => M)(using M: Modifier[F, swingio.MainFrame[F], M]): Resource[F, (swingio.MainFrame[F], Deferred[F, Unit])] =
+  override def withSelf[M](mkModifier: swingio.MainFrame[F] => M)(using
+      M: Modifier[F, swingio.MainFrame[F], M]
+  ): Resource[F, (swingio.MainFrame[F], Deferred[F, Unit])] =
     fullConstructor.flatTap((l, _) => M.modify(mkModifier(l), l))
 }
 private trait SwingElems[F[_]](using F: Async[F]) {
-  lazy val label: NormalElem[F, swingio.Label[F]] = NormalElem(swingio.Label(""))
+  lazy val label: NormalElem[F, swingio.Label[F]] = NormalElem(
+    swingio.Label("")
+  )
   lazy val window: MainElem[F] = MainElem(swingio.MainFrame[F])
-  lazy val flow: NormalElem[F, swingio.FlowPanel[F]] = NormalElem(swingio.FlowPanel[F](swingio.FlowPanel.Alignment.Center))
-  lazy val box = 
-    NormalElem[F, swingio.BoxPanel[F]](swingio.BoxPanel[F](swingio.Orientation.Vertical))
-  lazy val button: NormalElem[F, swingio.Button[F]] = NormalElem(swingio.Button[F])
-  lazy val textField: NormalElem[F, swingio.TextField[F]] = NormalElem(swingio.TextField[F])
-  lazy val checkbox: NormalElem[F, swingio.CheckBox[F]] = NormalElem(swingio.CheckBox[F])
-  lazy val radio: NormalElem[F, swingio.RadioButton[F]] = NormalElem(swingio.RadioButton[F])
-  def comboBox[A]: NormalElem[F, swingio.ComboBox[F, A]] = NormalElem(swingio.ComboBox[F, A])
-  def listView[A]: NormalElem[F, swingio.ListView[F, A]] = NormalElem(swingio.ListView[F][A])
+  lazy val flow: NormalElem[F, swingio.FlowPanel[F]] = NormalElem(
+    swingio.FlowPanel[F](swingio.FlowPanel.Alignment.Center)
+  )
+  lazy val box =
+    NormalElem[F, swingio.BoxPanel[F]](
+      swingio.BoxPanel[F](swingio.Orientation.Vertical)
+    )
+  lazy val button: NormalElem[F, swingio.Button[F]] = NormalElem(
+    swingio.Button[F]
+  )
+  lazy val textField: NormalElem[F, swingio.TextField[F]] = NormalElem(
+    swingio.TextField[F]
+  )
+  lazy val checkbox: NormalElem[F, swingio.CheckBox[F]] = NormalElem(
+    swingio.CheckBox[F]
+  )
+  lazy val radio: NormalElem[F, swingio.RadioButton[F]] = NormalElem(
+    swingio.RadioButton[F]
+  )
+  lazy val slider: NormalElem[F, swingio.Slider[F]] = NormalElem(
+    swingio.Slider[F]
+  )
+  def comboBox[A]: NormalElem[F, swingio.ComboBox[F, A]] = NormalElem(
+    swingio.ComboBox[F, A]
+  )
+  def listView[A]: NormalElem[F, swingio.ListView[F, A]] = NormalElem(
+    swingio.ListView[F][A]
+  )
   lazy val notebook =
     NormalElem[F, swingio.TabbedPane[F]](swingio.TabbedPane[F])
 }
--- swingio/src/main/scala/net/bulbyvr/swing/io/SwingProp.scala
+++ swingio/src/main/scala/net/bulbyvr/swing/io/SwingProp.scala
@@ -37,52 +37,69 @@ object SwingProp {
   trait Setter[F[_], E, A, V] {
     def set(elem: E, value: V): Resource[F, Unit]
   }
-  trait Emits[F[_], E, A, Ev] {} 
+  trait Emits[F[_], E, A, Ev] {}
   final class ConstantModifier[F[_], A, V] private[io] (
       private[io] val value: V
-    )
+  )
   final class SignalModifier[F[_], A, V] private[io] (
       private[io] val values: Signal[F, V]
-    )
+  )
   final class SignalResourceModifier[F[_], A, V] private[io] (
       private[io] val values: Resource[F, Signal[F, V]]
-    )
+  )
   final class ResourceModifier[F[_], A, V] private[io] (
       private[io] val value: Resource[F, V]
-    )
+  )
   final class OptionSignalModifier[F[_], A, V] private[io] (
       private[io] val values: Signal[F, Option[V]]
-    )
+  )
   final class OptionSignalResourceModifier[F[_], A, V] private[io] (
       private[io] val values: Resource[F, Signal[F, Option[V]]]
-    )
+  )
   final class PipeModifier[F[_], A, Ev] private[io] (
-    private[io] val sink: Pipe[F, Ev, Nothing]
-
-    )
-  private[io] def listener[F[_], T <: swingio.event.Event[F]](target: swingio.WithTopic[F])
-  (using F: Async[F], T: TypeTest[swingio.event.Event[F], T]): Stream[F, T] =
+      private[io] val sink: Pipe[F, Ev, Nothing]
+  )
+  private[io] def listener[F[_], T <: swingio.event.Event[F]](
+      target: swingio.WithTopic[F]
+  )(using F: Async[F], T: TypeTest[swingio.event.Event[F], T]): Stream[F, T] =
     target.topic.subscribe(5).collect { case e: T => e }
 }
 
 private trait PropModifiers[F[_]](using F: Async[F]) {
   import SwingProp.*
-  given forConstantProp[A, E, V](using S: Setter[F, E, A, V]): Modifier[F, E, ConstantModifier[F, A, V]] =
+  given forConstantProp[A, E, V](using
+      S: Setter[F, E, A, V]
+  ): Modifier[F, E, ConstantModifier[F, A, V]] =
     (m, n) => S.set(n, m.value)
-  given forSignalProp[E, A, V](using S: Setter[F, E, A, V]): Modifier[F, E, SignalModifier[F, A, V]] =
-    Modifier.forSignal[F, E, SignalModifier[F, A, V], V](_.values) { (m, n) => 
-      it => S.set(n, it)
-    }
-  given forSignalResource[E, A, V](using S: Setter[F, E, A, V]): Modifier[F, E, SignalResourceModifier[F, A, V]] =
-    Modifier.forSignalResource[F, E, SignalResourceModifier[F, A, V], V](_.values) {
+  given forSignalProp[E, A, V](using
+      S: Setter[F, E, A, V]
+  ): Modifier[F, E, SignalModifier[F, A, V]] =
+    Modifier.forSignal[F, E, SignalModifier[F, A, V], V](_.values) {
       (m, n) => it => S.set(n, it)
     }
-  given forResource[E, A, V](using S: Setter[F, E, A, V]): Modifier[F, E, ResourceModifier[F, A, V]] =
+  given forSignalResource[E, A, V](using
+      S: Setter[F, E, A, V]
+  ): Modifier[F, E, SignalResourceModifier[F, A, V]] =
+    Modifier.forSignalResource[F, E, SignalResourceModifier[F, A, V], V](
+      _.values
+    ) { (m, n) => it =>
+      S.set(n, it)
+    }
+  given forResource[E, A, V](using
+      S: Setter[F, E, A, V]
+  ): Modifier[F, E, ResourceModifier[F, A, V]] =
     (m, n) => m.value.flatMap(S.set(n, _))
 
-  given forPipeEventProp[E <: swingio.UIElement[F], A, Ev <: swingio.event.Event[F]](using T: TypeTest[swingio.event.Event[F], Ev])
-  : Modifier[F, E, PipeModifier[F, A, Ev]] = 
-    (m, t) => (F.cede *> listener[F, Ev](t).through(m.sink).compile.drain).background.void
+  given forPipeEventProp[E <: swingio.UIElement[
+    F
+  ], A, Ev <: swingio.event.Event[F]](using
+      T: TypeTest[swingio.event.Event[F], Ev]
+  ): Modifier[F, E, PipeModifier[F, A, Ev]] =
+    (m, t) =>
+      (F.cede *> listener[F, Ev](t)
+        .through(m.sink)
+        .compile
+        .drain).background.void
 
 }
 
@@ -92,97 +109,134 @@ private trait Props[F[_]](using A: Async[F]) extends LowPriorityProps[F] {
     SwingProp[F, A]
   def eventProp[A, Ev]: EventProp[F, A, Ev] =
     EventProp[F, A, Ev]
-  //given textBtn[E <: AbstractButton[F]]: Setter[F, E, "text", String] =
+  // given textBtn[E <: AbstractButton[F]]: Setter[F, E, "text", String] =
   //  (e, v) => e.text.set(v)
-  given textMost[E <: swingio.WithChangableText[F]]: Setter[F, E, "text", String] =
+  given textMost[E <: swingio.WithChangableText[F]]
+      : Setter[F, E, "text", String] =
     (e, v) => e.text.set(v).toResource
-  //given txtCompText[E <: TextComponent[F]]: Setter[F, E, "text", String] =
+  // given txtCompText[E <: TextComponent[F]]: Setter[F, E, "text", String] =
   //  (e, v) => e.text.set(v)
   lazy val text: SwingProp[F, "text"] = prop["text"]
-  given richWindowTitle[E <: swingio.RichWindow[F]]: Setter[F, E, "title", String] =
+  given richWindowTitle[E <: swingio.RichWindow[F]]
+      : Setter[F, E, "title", String] =
     (e, v) => e.title.set(v).toResource
   lazy val title: SwingProp[F, "title"] = prop["title"]
-  given rootPanelChild[E <: swingio.RootPanel[F], C <: swingio.Component[F]]: Setter[F, E, "child", C] =
+  given rootPanelChild[E <: swingio.RootPanel[F], C <: swingio.Component[F]]
+      : Setter[F, E, "child", C] =
     (e, v) => e.child.set(Some(v)).toResource
   lazy val child: SwingProp[F, "child"] =
     prop["child"]
+  lazy val min: SwingProp[F, "min"] = prop["min"]
+  given sliderMin[E <: swingio.Slider[F]]: Setter[F, E, "min", Int] =
+    (e, v) => e.min.set(v).toResource
+  lazy val max: SwingProp[F, "max"] = prop["max"]
+  given sliderMax[E <: swingio.Slider[F]]: Setter[F, E, "max", Int] =
+    (e, v) => e.max.set(v).toResource
+  lazy val value: SwingProp[F, "value"] = prop["value"]
+  given sliderValue[E <: swingio.Slider[F]]: Setter[F, E, "value", Int] =
+    (e, v) => e.value.set(v).toResource
 
   lazy val onBtnClick =
     eventProp["onBtnClick", swingio.event.ButtonClicked[F]]
 
-
   export swingio.Orientation
-  given orientationProp[E <: swingio.BoxPanel[F]]: Setter[F, E, "orientation", swingio.Orientation] =
+  given orientationProp[E <: swingio.BoxPanel[F]]
+      : Setter[F, E, "orientation", swingio.Orientation] =
     (e, v) => e.orientation.set(v).toResource
   lazy val orientation: SwingProp[F, "orientation"] =
     prop["orientation"]
 
-  def listening(listenTo: Pipe[F, swingio.event.Event[F], Nothing]): PipeModifier[F, "listen", swingio.event.Event[F]] =
+  def listening(
+      listenTo: Pipe[F, swingio.event.Event[F], Nothing]
+  ): PipeModifier[F, "listen", swingio.event.Event[F]] =
     PipeModifier(listenTo)
 
   export swingio.FlowPanel.Alignment as FlowAlignment
-  given alignmentProp[E <: swingio.FlowPanel[F]]: Setter[F, E, "alignment", swingio.FlowPanel.Alignment] =
+  given alignmentProp[E <: swingio.FlowPanel[F]]
+      : Setter[F, E, "alignment", swingio.FlowPanel.Alignment] =
     (e, v) => e.alignment.set(v).toResource
   lazy val alignment = prop["alignment"]
 
-  given selectedProp[E <: swingio.AbstractButton[F]]: Setter[F, E, "selected", Boolean] =
+  given selectedProp[E <: swingio.AbstractButton[F]]
+      : Setter[F, E, "selected", Boolean] =
     (e, v) => e.selected.set(v).toResource
   lazy val selected = prop["selected"]
 
-  given colsProp[E <: swingio.TextComponent.HasColumns[F]]: Setter[F, E, "columns", Int] =
+  given colsProp[E <: swingio.TextComponent.HasColumns[F]]
+      : Setter[F, E, "columns", Int] =
     (e, v) => e.columns.set(v).toResource
 
   lazy val columns = prop["columns"]
 
-  given rowsProp[E <: swingio.TextComponent.HasRows[F]]: Setter[F, E, "rows", Int] =
+  given rowsProp[E <: swingio.TextComponent.HasRows[F]]
+      : Setter[F, E, "rows", Int] =
     (e, v) => e.rows.set(v).toResource
 
   lazy val rows = prop["rows"]
 
-  given rendererProp[A, E <: swingio.WithRenderer[F, A]]: Setter[F, E, "renderer", A => F[String]] =
-    (e, v) => swingio.ListView.Renderer.text[F, A] {
-      (s: Boolean, f: Boolean, a: A, i: Int) => v(a)
-    }.evalMap(e.renderer.set)
-  given rendererPropNoEval[A, E <: swingio.WithRenderer[F, A]]: Setter[F, E, "renderer", A => String] =
-    (e, v) => swingio.ListView.Renderer.text[F, A] {
-      (s: Boolean, f: Boolean, a: A, i: Int) => v(a).pure
-    }.evalMap(e.renderer.set)
-  given rendererRealProp[A, E <: swingio.WithRenderer[F, A]]: Setter[F, E, "renderer", swingio.ListView.Renderer[F, A]] =
+  given rendererProp[A, E <: swingio.WithRenderer[F, A]]
+      : Setter[F, E, "renderer", A => F[String]] =
+    (e, v) =>
+      swingio.ListView.Renderer
+        .text[F, A] { (s: Boolean, f: Boolean, a: A, i: Int) =>
+          v(a)
+        }
+        .evalMap(e.renderer.set)
+  given rendererPropNoEval[A, E <: swingio.WithRenderer[F, A]]
+      : Setter[F, E, "renderer", A => String] =
+    (e, v) =>
+      swingio.ListView.Renderer
+        .text[F, A] { (s: Boolean, f: Boolean, a: A, i: Int) =>
+          v(a).pure
+        }
+        .evalMap(e.renderer.set)
+  given rendererRealProp[A, E <: swingio.WithRenderer[F, A]]
+      : Setter[F, E, "renderer", swingio.ListView.Renderer[F, A]] =
     (e, v) => e.renderer.set(v).toResource
   lazy val renderer = prop["renderer"]
 
-  given itemForComboBox[A, E <: swingio.ComboBox[F, A]]: Setter[F, E, "items", Seq[A]] =
+  given itemForComboBox[A, E <: swingio.ComboBox[F, A]]
+      : Setter[F, E, "items", Seq[A]] =
     (e, v) => e.items.set(v).toResource
   lazy val items = prop["items"]
 
-  lazy val onMouseClick = eventProp["onMouseClick", swingio.event.MouseClicked[F]]
+  lazy val onMouseClick =
+    eventProp["onMouseClick", swingio.event.MouseClicked[F]]
 
-  lazy val onSelectionChange = eventProp["onSelectionChange", swingio.event.SelectionChanged[F]]
+  lazy val onSelectionChange =
+    eventProp["onSelectionChange", swingio.event.SelectionChanged[F]]
 
-  lazy val onValueChange = eventProp["onValueChange", swingio.event.ValueChanged[F]]
+  lazy val onValueChange =
+    eventProp["onValueChange", swingio.event.ValueChanged[F]]
 
-  given iconProp[A, E <: swingio.WithIcon[F]]: Setter[F, E, "icon", swingio.Image[F]] =
+  given iconProp[A, E <: swingio.WithIcon[F]]
+      : Setter[F, E, "icon", swingio.Image[F]] =
     (e, v) => e.icon.set(Some(swingio.ImageIcon[F](v))).toResource
-  given iconPropReal[A, E <: swingio.WithIcon[F], I <: swingio.Icon[F]]: Setter[F, E, "icon", I] =
+  given iconPropReal[A, E <: swingio.WithIcon[F], I <: swingio.Icon[F]]
+      : Setter[F, E, "icon", I] =
     (e, v) => e.icon.set(Some(v)).toResource
-  given iconPropOption[A, E <: swingio.WithIcon[F], I <: swingio.Icon[F]]: Setter[F, E, "icon", Option[I]] =
+  given iconPropOption[A, E <: swingio.WithIcon[F], I <: swingio.Icon[F]]
+      : Setter[F, E, "icon", Option[I]] =
     (e, v) => e.icon.set(v).toResource
   lazy val icon = prop["icon"]
 
-  given lookAndFeelProp[E <: swingio.MainFrame[F]]: Setter[F, E, "lookAndFeel", String] =
-    (e, v) => A.delay { UIManager.setLookAndFeel(v) }.evalOn(AwtEventDispatchEC).toResource
+  given lookAndFeelProp[E <: swingio.MainFrame[F]]
+      : Setter[F, E, "lookAndFeel", String] =
+    (e, v) =>
+      A.delay { UIManager.setLookAndFeel(v) }
+        .evalOn(AwtEventDispatchEC)
+        .toResource
   lazy val lookAndFeel = prop["lookAndFeel"]
 
-  given adjustToProp[E <: swingio.Adjustable[F]]: Setter[F, E, "adjustTo", swingio.AdjustTo] =
+  given adjustToProp[E <: swingio.Adjustable[F]]
+      : Setter[F, E, "adjustTo", swingio.AdjustTo] =
     (e, v) => e.adjustTo.set(v).toResource
   lazy val adjustTo = prop["adjustTo"]
 }
 
-private trait LowPriorityProps[F[_]] (using F: Async[F]) {
+private trait LowPriorityProps[F[_]](using F: Async[F]) {
   import SwingProp.*
-  given compClick[E <: swingio.Component[F]]: Emits[F, E, "onClick", swingio.event.MouseClicked[F]] = new Emits {}
+  given compClick[E <: swingio.Component[F]]
+      : Emits[F, E, "onClick", swingio.event.MouseClicked[F]] = new Emits {}
 
 }
-
-
-
